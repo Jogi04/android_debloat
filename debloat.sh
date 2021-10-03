af2444382efd92520ed0ceb1814840df896c58c9
Jogi123 <testmail@test.com>
2021-10-03 17:11:11 +0200

    fixed syntax

--- debloat.sh
+++ debloat.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-sudo pacman -S android-tools --noconfirm --needed
 
-$apps = @(
+if [[ $(pacman -Q | grep android-tools) == "" ]]
+then
+  sudo pacman -S android-tools --noconfirm --needed
+fi
 
+apps=(
+  com.facebook.services
+  com.facebook.katana
+  com.facebook.system
+  com.facebook.appmanager
 )
 
-foreach ($app in $apps) {
+for app in ${apps[@]}
+do
     adb shell pm uninstall --user 0 $app
-}
+done
