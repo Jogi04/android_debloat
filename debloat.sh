e30a30ccb49f98d56a468caea2b7cfb5b450be19
Jogi123 <testmail@test.com>
2021-10-04 20:24:39 +0200

    added samsung location

--- debloat.sh
+++ debloat.sh
@@ -57,6 +57,7 @@ apps=(
   com.samsung.android.game.gos                                # Game Launcher
   com.samsung.android.kidsinstaller                           # Samsung Kids
   com.samsung.android.app.galaxyfinder                        # Finder
+  com.samsung.android.location
 
   # Google apps
   com.google.android.apps.tachyon                             # Google Duo
